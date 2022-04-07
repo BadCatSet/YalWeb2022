4e89eab5b60dd681d32a364c088e8440b6d8f519
Peteryarusov <94130861+Peteryarusov@users.noreply.github.com>
2022-04-07 22:09:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import datetime
-import logging
-from logging import critical, debug, error, info, warning
 import sqlite3
-
+#<div id="box"></div>
 from flask import Flask, redirect, render_template
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
@@ -10,13 +8,28 @@ from db import sql_gate
 from forms.login import LoginForm
 from forms.signup import SignupForm
 
-logging.basicConfig(
-    filename='log.log',
-    format='%(levelname)s %(asctime)s %(name)s >>> %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    level=logging.DEBUG,
-    encoding='utf-8')
-info('--- starting app -----------------------------------------')
+
+class User:
+    is_active = True
+    is_anonymous = False
+
+    def __init__(self, _id):
+        data = sql_gate.get_users(con, _id=_id)
+        if len(data) == 0:
+            self.is_authenticated = False
+        else:
+            self.is_authenticated = True
+            self.id, self.email, self.password_h, self.username = data[0]
+
+    def get_id(self):
+        return self.id
+
+    def __str__(self):
+        return f'username:{self.username} ' \
+               f'email:{self.email} ' \
+               f'auth:{self.is_authenticated} '
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'password_is_eight_asterisks'
 app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
@@ -25,41 +38,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-class AppError(Exception):
-    pass
-
-
-class User:
-    is_active = True
-    is_anonymous = False
-
-    def __init__(self, data):
-        debug(f'creating user object with {data}')
-
-        if not isinstance(data, tuple):
-            err = f'user object must receive a tuple not {type(data)}'
-            error(err)
-            raise AppError('err')
-
-        if len(data) == 0:
-            self.is_authenticated = False
-        else:
-            self.is_authenticated = True
-            self.id, self.email, self.password_h, self.username = data
-
-    def get_id(self):
-        return self.id
-
-    def __str__(self):
-        return f'''username:{self.username}    
-        email:{self.email}    
-        auth:{self.is_authenticated}'''
-
-
 @login_manager.user_loader
 def load_user(user_id):
-    user_data = sql_gate.get_users(con, user_id=user_id)[0]
-    return User(user_data)
+    return User(user_id)
 
 
 @app.route('/logout')
@@ -74,13 +55,18 @@ def index():
     return render_template('index.html', title='YalWeb2022')
 
 
+@app.route('/personal_account')
+def personal_account():
+    return render_template('personal_account.html', title='YalWeb2022')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = sql_gate.get_users(con, email=form.data['email'], password=form.data['password'])
         if len(user):
-            login_user(User(user[0]), remember=True)
+            login_user(User(user[0][0]), remember=True)
             return redirect('/')
     return render_template('login.html', title='Авторизация', form=form)
 
@@ -93,14 +79,11 @@ def signup():
         if len(data) != 0:
             form.email.errors.append('почта уже занята!')
         else:
-            sql_gate.add_user(con, form.data['email'], form.data['password1'])
+            sql_gate.add_user(con, form.data['email'], form.data['password'])
     return render_template('signup.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
-    info('connecting to database...')
     con = sqlite3.connect('db/db.db', check_same_thread=False)
-    info('...connected successful')
     app.run()
-    # no use, just for non-grey import
-    _ = current_user, debug, info, warning, error, critical
+    print(current_user)  # no use, just for non-grey import
