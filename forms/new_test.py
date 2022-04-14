fa2aa1e9cda91f8de9a18c8b93fd7ea744278680
Sergey207 <sergeypeckin207@gmail.com>
2022-04-14 20:17:31 +0300

    Закончил работу над стартовой страницей создания теста

--- forms/new_test.py
+++ forms/new_test.py
@@ -1,11 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, Field, FieldList, Label
+from wtforms import StringField, SubmitField, Field
 from wtforms.validators import DataRequired
 
 class newTestForm(FlaskForm):
     test_name = StringField("Введите название теста", validators=[DataRequired("")])
-    quantite_of_answers = IntegerField("Введите количество вопросов", validators=[DataRequired("")])
-    # types_of_answers = FieldList(Label(field_id=0, text="Типы вопросов"))
     submit = SubmitField("Продолжить")
 
     def get_placeholder(self, field: Field):
